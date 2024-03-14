66583fafc4d4b4f4dc0d5737825a6e343d94f703
djcsdj <djcsdj@hotmail.com>
2024-03-14 14:04:23 +0200

    fixed rules button

---
+++ rules-eng.js
@@ -0,0 +1,8 @@
+function displayRulesENG(){
+    var x = document.getElementById("rulesENG");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
